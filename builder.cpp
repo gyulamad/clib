e750007b29d567e348386b2acf2dcd52f8ca73c4
gyulamad <gyula.madarasz@pm.me>
2024-01-07 12:37:00 +0000

    show build time

--- builder.cpp
+++ builder.cpp
@@ -26,12 +26,19 @@ int main(int argc, const char* argv[]) {
         const string outputFolder = args_get_output_folder(args);
         const string hppCutFolder = args_get_hpp_cut_folder(args);
 
+        ms_t start_at = now();
+        cout << "Build starts at: " COLOR_DATETIME << ms_to_datetime(start_at) << COLOR_DEFAULT << endl;
+
         args_t build_args = args;
         if (hpp_cut) {
             hppcut(args); // TODO: do not override .h and .cpp files if already exists and newer that the source files
             build_args["input-filename"] = path_normalize(outputFolder + "/" + hppCutFolder + "/" + inputFilename);
         }
         if (!no_build) build(build_args);
+        
+        ms_t finish_at = now();
+        cout << "Build finish at: " COLOR_DATETIME << ms_to_datetime(finish_at) << COLOR_DEFAULT << endl;
+        cout << "Build in: " COLOR_HIGHLIGHT << finish_at - start_at << " ms" << COLOR_DEFAULT << endl;
 
     } catch (exception &e) {
         const string errmsg = "Exception in build process: " + string(e.what());
